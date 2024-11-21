bf14a48cca049a81e5332dc4ccd56bdaf0eaebb0
avwarrier <avwarrier@ucdavis.edu>
2024-11-21 11:56:39 -0800

    level 1 almost done

--- shattered-dimensions/scripts/platformer/killzone.gd
+++ shattered-dimensions/scripts/platformer/killzone.gd
@@ -1,7 +1,6 @@
 extends Area2D
 
-@onready var timer: Timer = $Timer
-
 func _on_body_entered(body: Node2D) -> void:
-	print("Death!")
-	get_tree().reload_current_scene()
+	body.lives -= 1
+	body.respawn()
+	#get_tree().reload_current_scene()
