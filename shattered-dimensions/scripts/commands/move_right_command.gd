1a23ad57260bb86ecb573f3d0ba1ef1ba1acb5fc
avwarrier <avwarrier@ucdavis.edu>
2024-11-21 01:31:40 -0800

    basic player created

---
+++ shattered-dimensions/scripts/commands/move_right_command.gd
@@ -0,0 +1,11 @@
+class_name MoveRightCommand
+extends Command
+
+
+func execute(character: Character) -> Status:
+	var input = character.movement_speed
+	character.sprite.play("run")
+	character.velocity.x = input
+	character.sprite.flip_h = false
+	character.change_facing(Character.Facing.RIGHT)
+	return Status.DONE
---
+++ shattered-dimensions/scripts/player/player.gd
@@ -0,0 +1,121 @@
+class_name Player
+extends Character 
+
+@export var health:int = 100
+
+var _damaged:bool = false
+var _dead:bool = false
+
+#@onready var animation_tree:AnimationTree = $AnimationTree
+
+
+func _ready():
+	#animation_tree.active = true
+	bind_player_input_commands()
+
+
+func _physics_process(delta: float):
+	if _dead:
+		return
+		
+	var move_input = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+
+	#if Input.is_action_just_pressed("attack"):
+		#fire1.execute(self)
+	
+	if move_input > 0.1:
+		right_cmd.execute(self)
+	elif move_input < -0.1:
+		left_cmd.execute(self)
+	else:
+		#if is_on_floor():
+		idle.execute(self)
+		#else:
+			#sprite.play("jump")
+	
+	if Input.is_action_just_pressed("jump"):
+		up_cmd.execute(self)
+	
+	super(delta)
+	#_manage_animation_tree_state()
+
+
+#func take_damage(damage:int) -> void:
+	#health -= damage
+	#_damaged = true
+	#if 0 >= health:
+		#_play($Audio/defeat)
+		#_dead = true
+		#animation_tree.active = false
+		#animation_player.play("death")
+	#else:
+		#_play($Audio/hurt)
+
+
+#Logic to support the state machine in the AnimationTree
+#func _manage_animation_tree_state() -> void:
+	#if !is_zero_approx(velocity.x):
+		#animation_tree["parameters/conditions/idle"] = false
+		#animation_tree["parameters/conditions/moving"] = true
+	#else:
+		#animation_tree["parameters/conditions/idle"] = true
+		#animation_tree["parameters/conditions/moving"] = false
+	#
+	#if is_on_floor():
+		#animation_tree["parameters/conditions/jumping"] = false
+		#animation_tree["parameters/conditions/on_floor"] = true
+	#else:
+		#animation_tree["parameters/conditions/jumping"] = true
+		#animation_tree["parameters/conditions/on_floor"] = false
+	#
+	##toggles
+	#if attacking:
+		#animation_tree["parameters/conditions/attacking"] = true
+		#attacking = false
+	#else:
+		#animation_tree["parameters/conditions/attacking"] = false
+	#
+	#if sword_up:
+		#animation_tree["parameters/conditions/sword_up"] = true
+		#sword_up = false
+	#else:
+		#animation_tree["parameters/conditions/sword_up"] = false
+		#
+	#if _damaged:
+		#animation_tree["parameters/conditions/damaged"] = true
+		#_damaged = false
+	#else:
+		#animation_tree["parameters/conditions/damaged"] = false
+
+
+func bind_player_input_commands():
+	right_cmd = MoveRightCommand.new()
+	left_cmd = MoveLeftCommand.new()
+	up_cmd = JumpCommand.new()
+	#fire1 = AttackCommand.new()
+	idle = IdleCommand.new()
+
+
+func unbind_player_input_commands():
+	right_cmd = Command.new()
+	left_cmd = Command.new()
+	up_cmd = Command.new()
+	fire1 = Command.new()
+	idle = Command.new()
+
+#func command_callback(cmd_name:String) -> void:
+	#if "attack" == cmd_name:
+		#_play($Audio/attack)
+		#
+	#if "jump" == cmd_name:
+		#_play($Audio/jump)
+		#
+	#if "engage" == cmd_name:
+		#_play($Audio/engage)
+		#
+	#if "undeath" == cmd_name:
+		#_play($Audio/undeath)
+
+func _play(player:AudioStreamPlayer2D) -> void:
+	if !player.playing:
+		player.play()
