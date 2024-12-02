fbd12da5bf0132c8e5189e3d3a625204c0d952f8
janania <37945416+janania@users.noreply.github.com>
2024-12-01 21:27:41 -0800

    enemy animations created

---
+++ shattered-dimensions/scripts/car/enemy/bat.gd
@@ -0,0 +1,14 @@
+extends Node2D
+
+var bat_attack_sprite: Sprite2D
+var bat_attack : AnimationPlayer
+
+var bat
+
+func _ready() -> void:
+	bat_attack_sprite = $BatAttack
+	bat_attack = $BatAttack/AnimationPlayer
+	bat_attack_sprite.flip_h = true
+
+func _process(delta: float) -> void:
+	bat_attack.play('attack')
