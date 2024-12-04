9d9a647621750cbb187fa97647d59d83e3a4c82d
srilakshmipanda <psldurga@gmail.com>
2024-12-04 00:31:24 -0800

    Tempp commit

--- shattered-dimensions/scripts/car/vehicles/swat_van.gd
+++ shattered-dimensions/scripts/car/vehicles/swat_van.gd
@@ -3,13 +3,16 @@ extends Vehicle
 
 var _defeated_by_player: bool
 var _showed_defeated_label: bool
+var width: float
 
 @onready var _escaped_label = preload("res://scenes/car/labels/defeated_police.tscn")
+@onready var camera: Camera2D = %CarCamera
 
 func _init() -> void:
 	character_type = Characters.Type.ENEMY
 	_defeated_by_player = false
 	_showed_defeated_label = false
+	width = get_viewport_rect().size.x / 2
 	#_speed = 600
 	#_max_speed = 60
 	super()
@@ -23,8 +26,9 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	if health > 0:
 		for wheel in _wheels:
-			if wheel.angular_velocity < _max_speed:
+			if wheel.angular_velocity < _max_speed or position.x < camera.position.x + width:
 				wheel.apply_torque_impulse(_speed * delta * 60)
+			
 	
 	if health <= 0 and _defeated_by_player and !_showed_defeated_label:
 		var escaped_label = _escaped_label.instantiate() as Label
@@ -32,11 +36,11 @@ func _physics_process(delta: float) -> void:
 		escaped_label.position.x = %Car.position.x + 300
 		escaped_label.position.y = %Car.position.y - 400
 		add_sibling(escaped_label)
-	
+		
 	%HealthBar.value = health
 	
 	super(delta)
 	
 func _delayed_action() -> void:
-	_speed = 600
-	_max_speed = 40
+	_speed = 400
+	_max_speed = 60
