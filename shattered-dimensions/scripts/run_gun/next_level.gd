c56d38dcf9583f4124970b6b2b41c51936afd402
AnunayAkhaury <anunay2003@gmail.com>
2024-12-08 14:54:24 -0800

    DEV: more moving logic + level transition

---
+++ shattered-dimensions/scripts/run_gun/next_level.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+@export var next_level_path: String = "res://scenes/run_gun/boss_level/boss_level.tscn"  # Path to the next level scene
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("player"):  
+		load_next_level()
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("player"):  
+		load_next_level()
+
+func load_next_level() -> void:
+	get_tree().change_scene_to_file(next_level_path) 
