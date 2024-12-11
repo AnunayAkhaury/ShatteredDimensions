cd83b2f9ad5b6fae98fe323613b825ca25f124ad
ssaneg <ssanegepalli@ucdavis.edu>
2024-12-11 14:04:24 -0800

    More updates

--- shattered-dimensions/scripts/menu_screens/game_menu.gd
+++ shattered-dimensions/scripts/menu_screens/game_menu.gd
@@ -3,7 +3,7 @@ extends Control
 @onready var instructions: Control = $Instructions2
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/prison.tscn")
+	get_tree().change_scene_to_file("res://scenes/intial_cutscene/intial_cutscene.tscn")
 
 
 func _on_instructions_pressed() -> void:
